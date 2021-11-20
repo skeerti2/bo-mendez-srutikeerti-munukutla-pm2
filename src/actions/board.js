22132cf37e41dd6c50dd790b00a6372d9af65593
bomendez <mendez.bo@husky.neu.edu>
2021-11-20 10:02:44 -0800

    Ships randomly placed and Restart Button replaces ships

--- src/actions/board.js
+++ src/actions/board.js
@@ -1,13 +1,14 @@
 import { BOARD_CLICK } from "./constants"
 
-export const boardClick= (x_coord, y_coord, player_id, hitShip) => {
+export const boardClick= (x_coord, y_coord, enemy, hit, miss) => {
     return {
         type: BOARD_CLICK,
         payload: {
             x_coord: x_coord,
             y_coord: y_coord,
-            player_id: player_id,
-            hitShip: hitShip
+            enemy: enemy,
+            hit: hit,
+            miss: miss,
         }
     }
 }
